269965abc1d4893e662d0b0f6f7edeeb4fe3bb74
backslashed <gabriel@stripetree.co.uk>
2014-06-10 22:42:08 +0100

    Added some docs

--- _IndiumView.js
+++ _IndiumView.js
@@ -4,7 +4,6 @@ define([
 	"dojo/query",
 	"dojo/dom-construct",
     "dijit/Destroyable",
-    "indium/_LinkFunctionFactory",
     "indium/_TextBindingsMixin",
     "indium/_AttributeBindingsMixin",
     "indium/_IndiumViewHelpers"
@@ -14,31 +13,43 @@ define([
 	query,
 	domConstruct,
     Destroyable,
-    _LinkFunctionFactory,
     _TextBindingsMixin,
     _AttributeBindingsMixin,
     _IndiumViewHelpers
 ) {
-	return declare("_IndiumView", [_LinkFunctionFactory, _TextBindingsMixin,
-        _AttributeBindingsMixin, Destroyable, _IndiumViewHelpers], {
+	return declare("indium/_IndiumView", [_TextBindingsMixin, _AttributeBindingsMixin,
+        _IndiumViewHelpers, Destroyable], {
 
+        /**
+         * @description Stores substitution data and linking functions
+         * @type {Dojo.Memory}
+         */
 		$bindingStore: null,
 
+        /**
+         * @description Constants for substitution matching on template
+         */
 		SUBSTITUTIONS_ALL: /\{\{([^\s\|\}]+)\|?([^\s\|\}]+)?\}\}/g,
 		SUBSTITUTIONS_FIRST:  /\{\{([^\s\|\}]+)\|?([^\s\|\}]+)?\}\}/,
 
+        /**
+         * @description Constructs _IndiumView, initiates and destroys $bindingStore
+         */
 		constructor: function () {
 			// create store
-
-            //this.own(this.$bindingStore);
+            // this.own(this.$bindingStore);
 		},
 
+        /**
+         * @description Dijit life-cycle method, builds, traverses, compiles and
+         * links DOM node to model and/or instance properties
+         */
 		buildRendering: function () {
 			this.inherited(arguments);
 			this.domNode = domConstruct.toDom(this.template);
 
             if (this.domNode.nodeType != 1) {
-                throw new Error("Invalid template!");
+                throw new Error("Invalid template, must have only one element wrapper as the top node!");
             }
 
             this._traverse([
@@ -50,23 +61,35 @@ define([
             this._link();
 		},
 
+        /**
+         * @description Any post-traverse DOM processing is handled during compilation
+         */
 		_compile: function () {
             this._createTextNodeBindings();
             this._createAttrBindings();
 		},
 
+        /**
+         * @description Links all substitutions to their corresponding model or
+         * instance properties
+         */
         _link: function () {
             // observe models or properties
         },
 
-		_traverse: function (actionFns, rootNode) {
+        /**
+         * @descriptions Traverses the template's DOM and performs a list of
+         * actions on each valid node
+         * @param actions {Array<Function>} Array of functions, gets node as parameter
+         * @param rootNode {HTMLElement} The root node for the traversal
+         */
+		_traverse: function (actions, rootNode) {
             var node;
 
 			if (!document.createTreeWalker) {
-                // Use traverse for IE8
 				node = rootNode.childNodes[0];
 				while (node != null) {
-                    this._callFunctions(actionFns, this, node);
+                    this._callFunctions(actions, this, node);
 
 					if (node.hasChildNodes()) {
 						node = node.firstChild;
@@ -78,10 +101,10 @@ define([
 					}
 				}
 			} else {
-                // Use TreeWalker
 				var walk = document.createTreeWalker(rootNode, NodeFilter.SHOW_ALL, null, false);
+
 				while (node = walk.nextNode()) {
-                    this._callFunctions(actionFns, this, node);
+                    this._callFunctions(actions, this, node);
 				}
 			}
 		}
--- _LinkFunctionFactory.js
+++ _LinkFunctionFactory.js
@@ -1,6 +1,6 @@
 /**
  var set = s.generate(this, s.SETTER_ATTRIBUTE, { node: "htmlelem", formatFn: B })
- set(4) -> [Arguments[2]]
+ set(4) -> [Arguments[2]], 0 - Config object .... 1 - New Value
  */
 
 define([
@@ -16,25 +16,53 @@ define([
 
         EMPTY_FORMAT_FN: function (value) { return value; },
 
+        /**
+         * @description Keeps all registered setter types
+         * @type {Object}
+         */
         _setters: {},
 
+        /**
+         * @description Creates available setters
+         */
         constructor: function () {
-            this._addSetter(this.SETTER_ATTRIBUTE, this.setNodeAttribute);
-            this._addSetter(this.SETTER_TEXTNODE, this.setNodeValue);
+            this._registerSetter(this.SETTER_ATTRIBUTE, this.setNodeAttribute);
+            this._registerSetter(this.SETTER_TEXTNODE, this.setNodeValue);
         },
 
+        /**
+         * @description Generates a new setter function of the given type.
+         * Setter functions take a single value attribute and set it on the
+         * node that they have been linked to using the setting type defined.
+         * @param context {Object} The context in which the setter should run
+         * @param type {Number} The setter type (ie. SETTER_ATTRIBUTE, etc.)
+         * @param configObj {Object} Node, formatFn... depending on setter type
+         * @returns {function(value)} Return a setter function
+         */
         generate: function (context, type, configObj) {
             var setter = this._setters[type];
-
             return function (value) {
                 setter.call(context, arguments)
             }.bind(context, configObj);
         },
 
-        _addSetter: function (type, fn) {
+        /**
+         * Registers a setter function on the object
+         * @param type {Number} Setter type (ie. SETTER_REPEATER, etc.)
+         * @param fn {Function} The function corresponding to this type
+         * of setter
+         * @private
+         */
+        _registerSetter: function (type, fn) {
             this._setters[type] = fn;
         },
 
+        /**
+         * @desription Node attribute setter function
+         * @param args {Array<mixed>} Contains two items:
+         * new value to be set on the attribute and a configuration
+         * object describing specifics set during generation
+         */
         setNodeAttribute: function(args) {
             /*
             {
@@ -48,6 +76,12 @@ define([
             var value = args[1], data = args[0];
         },
 
+        /**
+         * @description Sets a text node's value
+         * @param args {Array<mixed>} Contains two items:
+         * new value to be set on the text node and a configuration
+         * object describing specifics set during generation
+         */
         setNodeValue: function (args) {
             var value = args[1], data = args[0],
                 formatFn = data.formatFn || this.EMPTY_FORMAT_FN;
