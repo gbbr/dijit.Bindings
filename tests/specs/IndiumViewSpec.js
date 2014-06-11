af71e00bf3d648fff20352ee0e364e7b2153e567
backslashed <gabriel@stripetree.co.uk>
2014-06-11 20:58:12 +0100

    Added test test framework

---
+++ tests/specs/IndiumViewSpec.js
@@ -0,0 +1,6 @@
+
+describe("IndiumView should compile and link template to instance and model properties", function () {
+	it("Should conver template to DOM tree and call comile function", function() {
+
+	});
+});
