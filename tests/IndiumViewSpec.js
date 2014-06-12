2e3940bc069bc40b18b7cb973f0623c8df762bec
backslashed <gabriel@stripetree.co.uk>
2014-06-12 18:34:42 +0100

    stuff

--- tests/IndiumViewSpec.js
+++ tests/IndiumViewSpec.js
@@ -19,21 +19,4 @@ define([
 			doh.assertTrue(w._applyCompilers.calledOnce);
 		}
 	});
-
-//	doh.register("view", [
-//		{
-//			name: "Builds DOM based on template and calls compile",
-//
-//			setUp: function () {},
-//
-//			runTest: function () {
-//				var w = TestWidgets.WIDGET_WITH_BINDINGS();
-//				w._applyCompilers = sinon.spy();
-//				w.compile(w.domNode);
-//				doh.assertTrue(w._applyCompilers.calledOnce);
-//			},
-//
-//			tearDown: function () {}
-//		}
-//	]);
 });
