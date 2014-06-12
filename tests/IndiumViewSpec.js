8fd24a86c471dfd53ba673fe21ca95a409c87e0a
backslashed <gabriel@stripetree.co.uk>
2014-06-12 18:33:33 +0100

    test suite

--- tests/IndiumViewSpec.js
+++ tests/IndiumViewSpec.js
@@ -3,26 +3,37 @@ require(["sinon"]);
 define([
 	"doh/runner",
 	"indium/_IndiumView",
-	"indium/tests/templates/TestWidgets"
+	"indium/tests/templates/TestWidgets",
+	"testSuite"
 ], function (
 	doh,
 	IndiumView,
-	TestWidgets
+	TestWidgets,
+	testSuite
 ) {
-	doh.register("view", [
-		{
-			name: "Builds DOM based on template and calls compile",
-
-			setUp: function () {},
-
-			runTest: function () {
-				var w = TestWidgets.WIDGET_WITH_BINDINGS();
-				w._applyCompilers = sinon.spy();
-				w.compile(w.domNode);
-				doh.assertTrue(w._applyCompilers.calledOnce);
-			},
-
-			tearDown: function () {}
+	testSuite("view", {
+		"Compiles DOM correctly": function () {
+			var w = TestWidgets.WIDGET_WITH_BINDINGS();
+			w._applyCompilers = sinon.spy();
+			w.compile(w.domNode);
+			doh.assertTrue(w._applyCompilers.calledOnce);
 		}
-	]);
+	});
+
+//	doh.register("view", [
+//		{
+//			name: "Builds DOM based on template and calls compile",
+//
+//			setUp: function () {},
+//
+//			runTest: function () {
+//				var w = TestWidgets.WIDGET_WITH_BINDINGS();
+//				w._applyCompilers = sinon.spy();
+//				w.compile(w.domNode);
+//				doh.assertTrue(w._applyCompilers.calledOnce);
+//			},
+//
+//			tearDown: function () {}
+//		}
+//	]);
 });
