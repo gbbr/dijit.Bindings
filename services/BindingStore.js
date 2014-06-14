4fd21781d5ac1db7da2040d583d885ee9a541d33
backslashed <gabriel@stripetree.co.uk>
2014-06-14 18:54:45 +0100

    Added BindingStore.js

---
+++ services/BindingStore.js
@@ -0,0 +1,41 @@
+define([
+	"dojo/_base/declare",
+	"dojo/store/Memory",
+	"dijit/Destroyable"
+], function (
+	declare,
+	Memory,
+	Destroyable
+) {
+	return declare("RegistrationService.StorageService", [Destroyable], {
+		/**
+		 * @description Stores substitution data and linking functions
+		 * @type {dojo/store/Memory}
+		 */
+		$bindingStore: null,
+
+		_collectorStore: [],
+
+		constructor: function () {
+			this.$bindingStore = new Memory();
+			this.own(this.$bindingStore);
+		},
+
+		createBindingStore: function () {
+			return this.$bindingStore;
+		},
+
+		attachSetter: function (name, fn) {
+			if (!this.$bindingStore.hasOwnProperty(name)) {
+				this.$bindingStore[name] = {};
+				this.$bindingStore[name].setters = [];
+			}
+
+			this.$bindingStore[name].setters.push(fn);
+		}
+
+		// get binding by name
+		// get binding by type
+		// ...
+	});
+});
