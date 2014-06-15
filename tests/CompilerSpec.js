bcdfbd47141e6c8832a6feaee4adcd112e279c70
backslashed <gabriel@stripetree.co.uk>
2014-06-15 14:47:53 +0100

    Improved compiler spec

--- tests/CompilerSpec.js
+++ tests/CompilerSpec.js
@@ -2,7 +2,7 @@ define([
 	"doh/runner",
 	"dojo/dom-construct",
 	"indium/services/Compiler",
-	'dojo/text!indium/tests/example_widget/template.html',
+	'dojo/text!indium/tests/templates/template1.html',
 	"testSuite"
 ], function (
 	doh,
@@ -14,10 +14,12 @@ define([
 	testSuite("Compiler service", {
 		beforeEach: function () {
 			this.instance = new Compiler();
+			this.templateDom = domConstruct.toDom(template);
 		},
 
 		afterEach: function () {
 			this.instance.destroy();
+			delete this.templateDom;
 		},
 
 		"Correctly invokes a list of functions": function () {
@@ -31,10 +33,44 @@ define([
 			});
 		},
 
-		"Correctly clears collector store and subscribers after building phase": function () {
-//			var dom = domConstruct.toDom(template);
-//			console.log(this.instance.compile(dom)(this));
-//			console.log(this.instance.registrationService.$bindingStore);
+		"Correctly clears collector store after building phase": function () {
+			this.spy(this.instance.registrationService, "clearCollected");
+
+			this.instance.compile(this.templateDom);
+
+			testSuite.isTrue(this.instance.registrationService.clearCollected.calledOnce,
+				"Compiling phase did not clear collector store");
+		},
+
+		"Invokes all registered collectors": function () {
+			var spies = [this.spy(), this.spy(), this.spy()];
+
+			this.instance.registrationService.addCollector("COLLECTOR_0", spies[0]);
+			this.instance.registrationService.addCollector("COLLECTOR_1", spies[1]);
+			this.instance.registrationService.addCollector("COLLECTOR_2", spies[2]);
+
+			this.instance.compile(this.templateDom);
+
+			testSuite.isTrue(spies[0].called, "Collector #1 was not reached");
+			testSuite.isTrue(spies[1].called, "Collector #2 was not reached");
+			testSuite.isTrue(spies[2].called, "Collector #3 was not reached");
+
+			testSuite.equals(spies[0].callCount, spies[1].callCount, "Call counts not equal");
+			testSuite.equals(spies[1].callCount, spies[2].callCount, "Call counts not equal");
+		},
+
+		"Invokes all registered builders once": function () {
+			var spies = [this.spy(), this.spy(), this.spy()];
+
+			this.instance.registrationService.addBuilder(spies[0]);
+			this.instance.registrationService.addBuilder(spies[1]);
+			this.instance.registrationService.addBuilder(spies[2]);
+
+			this.instance.compile(this.templateDom);
+
+			testSuite.isTrue(spies[0].calledOnce, "Builder #1 was not reached");
+			testSuite.isTrue(spies[1].calledOnce, "Builder #2 was not reached");
+			testSuite.isTrue(spies[2].calledOnce, "Builder #3 was not reached");
 		}
 	});
 });
