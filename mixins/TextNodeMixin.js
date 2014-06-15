fe3cd505ce25b71874a9682507402c90d6c7529a
backslashed <gabriel@stripetree.co.uk>
2014-06-15 15:04:06 +0100

    Documentation

--- mixins/TextNodeMixin.js
+++ mixins/TextNodeMixin.js
@@ -15,8 +15,15 @@ define([
 		COLLECTOR_TEXT_NODES: "GATHERER_TEXTNODES",
 		SETTER_TEXT_NODES: "SETTER_TEXTNODES",
 
+		/**
+		 * @description Store for collected data
+		 * @type {Array<HTMLElement>}
+		 */
 		_textNodeCollectorStore: null,
 
+		/**
+		 * @description Creates a store for the collectors, attaches them, adds builders and setters
+		 */
 		constructor: function () {
 			this._textNodeCollectorStore = this.registrationService.getCollectorStore(this.COLLECTOR_TEXT_NODES);
 
@@ -37,8 +44,9 @@ define([
 		},
 
 		/**
-		 * @description Compiles marked text-nodes and generates linking
-		 * functions
+		 * @description Interpolates text-node, creates bindings and if necessary
+		 * splits text-node into a fragment (when it is composed of multiple parts
+		 * or contains multiple expressions)
 		 */
 		_compileTextNodes: function () {
 			this._textNodeCollectorStore.forEach(function (node) {
@@ -57,6 +65,13 @@ define([
 			}, this);
 		},
 
+		/**
+		 * @description Creates a document fragment from an array of
+		 * parts and registers setters for expressions
+		 * @param parts {Array<String>} An array of parts in the desired order
+		 * @param expressions {Array<String>} An array of expressions found in the parts
+		 * @returns {DocumentFragment} The completed and bound document fragment
+		 */
 		_createBindingsFromFragment: function (parts, expressions) {
 			var fragment = document.createDocumentFragment();
 
@@ -72,6 +87,11 @@ define([
 			return fragment;
 		},
 
+		/**
+		 * @description Attaches a setter to the binding store to link to
+		 * @param expression {String} The expression that the setter is for
+		 * @param node {HTMLElement} The node where the expression should be evaluated
+		 */
 		_registerTextNodeSetter: function (expression, node) {
 			var parsedExpr = this.parseExpression(expression),
 				setterFn = this.registrationService.getSetter(this, this.SETTER_TEXT_NODES, {
