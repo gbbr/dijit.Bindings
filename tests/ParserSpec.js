87ea09c36536f9c7aa58b0f7bd4322eba21bde7a
backslashed <gabriel@stripetree.co.uk>
2014-06-21 15:43:34 +0100

    Clean Parser Spec

--- tests/ParserSpec.js
+++ tests/ParserSpec.js
@@ -16,110 +16,63 @@ define([
 			this.instance = new Parser();
 		},
 
-		"Correctly counts number of bindings": function () {
-			var testCases = [
-				["asd{{1}}zxc", 			 1],
-				["{{1}}", 					 1],
-				["{{1}}{{2}}{{3}}poi", 		 3],
-				["{{1}}ips|}um{{2}}", 		 2],
-				["{{1}}{{2}}x{{3}} asd ", 	 3],
-				["   {{1}}  {{3}}", 		 2],
-				["sadklj 90{}", 			 0],
-				["{{sadasdsdwq{{", 			 0,
-				["{}}sdjxck{{ }as"], 		 0],
-				["asd{{1|2}}", 				 1],
-				["{{1|4er}}", 				 1],
-				["{{1|asd2}}|{{|2|1}}{{3}}", 2],
-				["{{1|asd}}ipsum{{2|123}}",  2]
-			];
-
-			testCases.forEach(function (test) {
-				testSuite.equals(test[1], this.instance._bindingCount(test[0]), "Binding count failed at " + test[1]);
-			}, this);
+		"bindingCount: Correctly counts number of bindings": function () {
+			testSuite.equals(this.instance._bindingCount("{{name}}"), 1);
+			testSuite.equals(this.instance._bindingCount("{{name}}{{age}}"), 2);
+			testSuite.equals(this.instance._bindingCount("{{person.name|upperCase}} is {{person.age}} years old"), 2);
+			testSuite.equals(this.instance._bindingCount("Hello {{name|upperCase}} !"), 1);
+			testSuite.equals(this.instance._bindingCount("No expressions"), 0);
 		},
 
-		"Should not return undefined when no bindings are found": function () {
-			testSuite.equals(0, this.instance._bindingCount("asd"), "Did not return 0 at mismatched type")
+		"parseExpression: should return property and formatFn": function () {
+			testSuite.equals(this.instance.parseExpression("{{name|toUppercase}}"), {
+				expression: "name",
+				formatFn: "toUppercase"
+			});
+
+			testSuite.equals(this.instance.parseExpression("{{model.key}}"), {
+				expression: "model.key",
+				formatFn: void 0
+			});
+
+			testSuite.equals(this.instance.parseExpression("{{model.key|transformFn}}"), {
+				expression: "model.key",
+				formatFn: "transformFn"
+			});
+
+			testSuite.equals(this.instance.parseExpression("Nothing here{{}}"), void 0);
+			testSuite.equals(this.instance.parseExpression(""), void 0);
 		},
 
-		"Parse expression: should return binding name and formatFn or undefined": function () {
-			var testCases = [
-					[ "{{A|B}}", { expression: "A", formatFn: "B" } ],
-					[ "random text", void 0 ],
-					[ "", void 0 ],
-					[ "{{model.key|formatFn}}", { expression: "model.key", formatFn: "formatFn" } ],
-					[ "{{}}", void 0 ],
-					[ "{{model.key}}", { expression: "model.key", formatFn: void 0 } ],
-					[ "{{|formatFn}}", void 0 ],
-					[ "{{color}}", { expression: "color", formatFn: void 0 } ]
-				], result;
+		"interpolateString: should return expressions, separators and parts": function () {
+			var interpolateFn = this.instance.interpolateString("{{id}}");
 
-			testCases.forEach(function (test) {
-				result = this.instance.parseExpression(test[0]);
-				testSuite.equals(test[1], result, "Did not correctly parse " + test[0]);
-			}, this);
-		},
+			testSuite.equals(interpolateFn.expressions, ["{{id}}"]);
+			testSuite.equals(interpolateFn.separators, []);
+			testSuite.equals(interpolateFn.parts, ["{{id}}"]);
 
-		"Interpolate string: should not miss any expressions or separators": function () {
-			var testCases = [
-				["{{A}}", 				 {
-					separators: [],
-					expressions: ['{{A}}'],
-					parts: ["{{A}}"]
-				}],
-				["{{A}} ", 				 {
-					separators: [' '],
-					expressions: ['{{A}}'],
-					parts: ["{{A}}", " "]
-				}],
-				[" {{A}} ", 			 {
-					separators: [' ',' '],
-					expressions: ['{{A}}'],
-					parts: [" ", "{{A}}", " "]
-				}],
-				["{{A}}{{A}}", {
-					separators: [],
-					expressions: ['{{A}}', '{{A}}'],
-					parts: ['{{A}}', '{{A}}']
-				}],
-				["{{B}}x{{B}}y{{B}}", {
-					separators: ['x', 'y'],
-					expressions: ['{{B}}', '{{B}}', '{{B}}'],
-					parts: ['{{B}}', 'x', '{{B}}', 'y', '{{B}}']
-				}],
-				["{{A}}{{B}}", 			 {
-					separators: [],
-					expressions: ['{{A}}', '{{B}}'],
-					parts: ["{{A}}", "{{B}}"]
-				}],
-				["{{prop}} space {{model.key}}", 	 {
-					separators: [' space '],
-					expressions: ['{{prop}}', '{{model.key}}'],
-					parts: ["{{prop}}", " space ", "{{model.key}}"]
-				}],
-				["{{A|transformFn}}asd", {
-					separators: ['asd'],
-					expressions: ['{{A|transformFn}}'],
-					parts: ["{{A|transformFn}}", "asd"]
-				}],
-				[" {{prop.key|fn}}asd",  {
-					separators: [' ', 'asd'],
-					expressions: ['{{prop.key|fn}}'],
-					parts: [" ", "{{prop.key|fn}}", "asd"]
-				}],
-				["zxc {{A}} abc {{B}} qwe", {
-					separators: ["zxc ", " abc ", " qwe"],
-					expressions: ['{{A}}', '{{B}}'],
-					parts: ["zxc ", "{{A}}", " abc ", "{{B}}", " qwe"]
-				}]
-			];
+			interpolateFn = this.instance.interpolateString("{{id}}{{title}}");
 
-			testCases.forEach(function (test) {
-				var interpolateFn = this.instance.interpolateString(test[0]);
-				testSuite.equals(test[1].separators, interpolateFn.separators, "Separators did not match on " + test[0]);
-				testSuite.equals(test[1].expressions, interpolateFn.expressions, "Expressions did not match on " + test[0]);
-				testSuite.equals(test[1].parts, interpolateFn.parts, "Parts did not match on " + test[0]);
-			}, this);
+			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
+			testSuite.equals(interpolateFn.separators, []);
+
+			interpolateFn = this.instance.interpolateString("{{id}} and {{title}}");
+
+			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
+			testSuite.equals(interpolateFn.separators, [" and "]);
+			testSuite.equals(interpolateFn.parts, ["{{id}}", " and ", "{{title}}"]);
+
+			interpolateFn = this.instance.interpolateString("With {{id}} and {{title}}.");
+
+			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
+			testSuite.equals(interpolateFn.separators, ["With ", " and ", "."]);
+			testSuite.equals(interpolateFn.parts, ["With ", "{{id}}", " and ", "{{title}}", "."]);
+
+			interpolateFn = this.instance.interpolateString("With {{client.id}} and {{client.title|toUppercase}}.");
+
+			testSuite.equals(interpolateFn.expressions, ["{{client.id}}", "{{client.title|toUppercase}}"]);
+			testSuite.equals(interpolateFn.separators, ["With ", " and ", "."]);
+			testSuite.equals(interpolateFn.parts, ["With ", "{{client.id}}", " and ", "{{client.title|toUppercase}}", "."]);
 		},
 
 		"Interpolate function: Should correctly do replacements and react to unavailable data": function () {
@@ -127,16 +80,30 @@ define([
 					return str.toUpperCase();
 				},
 
-				dummyFunction = function () {},
-
 				testCases = [
-					["Hello {{name}} !", { name: "John" }, "Hello John !"],
-					["Today is {{day|uppercase}}", { day: "FriDay", uppercase: toUppercase }, "Today is FRIDAY"],
-					["{{a}}", { a: 2 }, "2"],
-					["{{value|inexistingFn}}tastic", { value: "output" }, "outputtastic"],
-					["{{foo}}{{bar|toUppercase}}", { toUppercase: dummyFunction }, "{{foo}}{{bar|toUppercase}}"],
-					["{{foo|toUppercase}}{{bar}}", { foo: "loo", toUppercase: toUppercase }, "LOO{{bar}}"],
-					["Hello {{myModel.key}}", { myModel: new _StatefulModel({ "key": "world" }) }, "Hello world"]
+					["Hello {{name}} !", { name: "John" },
+						"Hello John !"],
+
+					["Today is {{day|uppercase}}", { day: "FriDay", uppercase: toUppercase },
+						"Today is FRIDAY"],
+
+					["{{day|timesTwo}}nd of {{month}}", { day: 11, month: "July", timesTwo: function(x) {return x*2} },
+						"22nd of July"],
+
+					["{{value|inexistingFn}}tastic", { value: "fan" },
+						"fantastic"],
+
+					["{{foo}} {{bar|toUppercase}}", { bar: "boo" },
+						"{{foo}} boo"],
+
+					["{{foo|toUppercase}}{{bar}}", { foo: "loo", toUppercase: toUppercase },
+						"LOO{{bar}}"],
+
+					["Error {{item|forgotToReturn}}", { item: "you forgot to return", forgotToReturn: function (val) {} },
+						"Error undefined"],
+
+					["Hello {{myModel.key}}", { myModel: new _StatefulModel({ "key": "world" }) },
+						"Hello world"]
 				];
 
 			testCases.forEach(function (test) {
