35b0e63e8568dbb77ff24e7cdca549c455201149
backslashed <gabriel@stripetree.co.uk>
2014-06-21 20:34:49 +0100

    ParserSpec

--- tests/ParserSpec.js
+++ tests/ParserSpec.js
@@ -46,38 +46,33 @@ define([
 
 		"interpolateString: should return expressions, separators and parts": function () {
 			var interpolateFn = this.instance.interpolateString("{{id}}");
-
 			testSuite.equals(interpolateFn.expressions, ["{{id}}"]);
 			testSuite.equals(interpolateFn.separators, []);
 			testSuite.equals(interpolateFn.parts, ["{{id}}"]);
 
 			interpolateFn = this.instance.interpolateString("{{id}}{{title}}");
-
 			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
 			testSuite.equals(interpolateFn.separators, []);
 			testSuite.equals(interpolateFn.parts, ["{{id}}", "{{title}}"]);
 
 			interpolateFn = this.instance.interpolateString("{{id}} and {{title}}");
-
 			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
 			testSuite.equals(interpolateFn.separators, [" and "]);
 			testSuite.equals(interpolateFn.parts, ["{{id}}", " and ", "{{title}}"]);
 
 			interpolateFn = this.instance.interpolateString("With {{id}} and {{title}}.");
-
 			testSuite.equals(interpolateFn.expressions, ["{{id}}", "{{title}}"]);
 			testSuite.equals(interpolateFn.separators, ["With ", " and ", "."]);
 			testSuite.equals(interpolateFn.parts, ["With ", "{{id}}", " and ", "{{title}}", "."]);
 
 			interpolateFn = this.instance.interpolateString("With {{client.id}} and {{client.title|toUppercase}}.");
-
 			testSuite.equals(interpolateFn.expressions, ["{{client.id}}", "{{client.title|toUppercase}}"]);
 			testSuite.equals(interpolateFn.separators, ["With ", " and ", "."]);
 			testSuite.equals(interpolateFn.parts, ["With ", "{{client.id}}", " and ", "{{client.title|toUppercase}}", "."]);
 		},
 
 		"Interpolate function: should not fail with HTML": function () {
-			interpolateFn = this.instance.interpolateString('<div class="{{className}}">{{body}} and {{legs}}.</div>');
+			var interpolateFn = this.instance.interpolateString('<div class="{{className}}">{{body}} and {{legs}}.</div>');
 
 			testSuite.equals(interpolateFn.expressions, ["{{className}}", "{{body}}", "{{legs}}"]);
 			testSuite.equals(interpolateFn.separators, ['<div class="', '">', ' and ', '.</div>']);
@@ -96,30 +91,47 @@ define([
 				},
 
 				testCases = [
+					// Test:
 					["Hello {{name}} !", { name: "John" },
+						// Expected result:
 						"Hello John !"],
 
+					// Test:
 					["Today is {{day|uppercase}}", { day: "FriDay", uppercase: toUppercase },
+						// Expected result:
 						"Today is FRIDAY"],
 
+					// Test:
 					["{{day|timesTwo}}nd of {{month}}", { day: 11, month: "July", timesTwo: function(x) {return x*2} },
+						// Expected result:
 						"22nd of July"],
 
+					// Test:
 					["{{value|inexistingFn}}tastic", { value: "fan" },
+						// Expected result:
 						"fantastic"],
 
+					// Test:
 					["{{foo}} {{bar|toUppercase}}", { bar: "boo" },
+						// Expected result:
 						"{{foo}} boo"],
 
+					// Test:
 					["{{foo|toUppercase}}{{bar}}", { foo: "loo", toUppercase: toUppercase },
+						// Expected result:
 						"LOO{{bar}}"],
 
+					// Test:
 					["Error {{item|forgotToReturn}}", { item: "you forgot to return", forgotToReturn: function (val) {} },
+						// Expected result:
 						"Error undefined"],
 
+					// Test:
 					["Hello {{myModel.key}}", { myModel: new _StatefulModel({ "key": "world" }) },
+						// Expected result:
 						"Hello world"]
 				];
+
 
 			testCases.forEach(function (test) {
 				testSuite.equals(test[2], this.instance.interpolateString(test[0])(test[1]),
