faea720e5aa98db40325a53db2a9a3faa1ca8d42
backslashed <gabriel@stripetree.co.uk>
2014-06-23 01:10:04 +0100

    doh

--- integration/src/main/js/view/bindings/BindingStore.js
+++ integration/src/main/js/view/bindings/BindingStore.js
@@ -38,17 +38,36 @@ define([
 		 * @description Creates a setter and binds a configuration to it
 		 * @param name {string} Name of the binding this setter belongs too
 		 * @param fn {Function} Setter function
-		 * @param config {object} Configuration object to bind to setter function
 		 */
 		createSetter: function (name, fn, config) {
 			if (!this.$bindingStore.get(name)) {
 				this.$bindingStore.put(lang.mixin({
 					id: name,
 					setters: []
-				}));
+				}, this._detectBindingType(name)));
 			}
 
 			this.$bindingStore.get(name).setters.push(fn.bind(this, config));
+		},
+
+		/**
+		 * Extends the binding store object by returning binding type
+		 * and additional information
+		 * @param {string} name
+		 * @returns {{type: string, model: Object, key: *}}
+		 */
+		_detectBindingType: function (name) {
+			var parts = name.split("."),
+				obj = lang.getObject(parts[0], false, this),
+				isModel = obj && lang.isFunction(obj.get) && !!parts[1];
+
+			return isModel ? {
+				type: this.objectType.MODEL,
+				model: obj,
+				key: parts[1]
+			} : {
+				type: this.objectType.PROPERTY
+			}
 		}
 	});
 });
