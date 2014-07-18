3bf402851f453c6596a65371a49d59affa5b00e5
Gabriel Aszalos <gabriel.aszalos@thomsonreuters.com>
2014-07-18 09:50:04 +0100

    Latest update

--- integration/src/test/js/view/bindings/module.js
+++
@@ -1,7 +0,0 @@
-define([
-	"test/view/bindings/BindingStore",
-	"test/view/bindings/Compiler",
-	"test/view/bindings/Parser",
-	"test/view/bindings/RegistrationService",
-	"test/view/bindings/TextNodeMixin"
-], function () {});
